7bb2ce7ab7b603b9353fcd37e3e80954edebbcf9
NobleExecutor <brakes_juniors.0t@icloud.com>
2025-11-18 14:21:08 +0100

    pleins de trucs et finito le script de l'index

--- js/script.js
+++ js/script.js
@@ -1,16 +1,7 @@
-function convTableau(path) {
-    let stringBrut = Object.values(path);
-    console.log(stringBrut);
-    let string = "";
-    for (let i = 0; i < stringBrut.length; i++) {
-        string += stringBrut[i];
-    }
-    console.log(string);
-    return string;
-}
-
-let pathTitre = SAE['SAE1.01']['titre'];
-let pathComp = SAE['SAE1.01']['compétences'];
-let pathDesc = SAE['SAE1.01']['description'];
-
-document.querySelector(".container-sae").innerHTML = "<h2>SAE1.01</h2><h3>Titre: " + convTableau(pathTitre) + "</h3><h3>Compétence: " + convTableau(pathComp) + "</h3><h3>Description: " + convTableau(pathDesc) + "</h3>";
+document.querySelectorAll('.container-sae').forEach(div => {
+    console.log(div.id, SAE[div.id]);
+    div.innerHTML += "<h2>" + div.id + "</h2>";
+    div.innerHTML += "<h3>Titre: " + SAE[div.id].titre + "</h3>";
+    div.innerHTML += "<h3>Compétence(s): " + SAE[div.id].compétences + "</h3>";
+    div.innerHTML += "<a class='plus-btn' href='pages/" + div.id + ".html'>En savoir plus</a>";
+});
