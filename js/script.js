ab9d053385d819ffc71d382547df5ff650478716
NobleExecutor <brakes_juniors.0t@icloud.com>
2025-11-25 11:09:11 +0100

    Refonte complète du JS, utilisation de paramètre dans les redirections

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('.container-sae').forEach(div => {
-    console.log(div.id, SAE[div.id]);
-    div.innerHTML += "<h2>" + div.id + "</h2>";
-});
+for (let num in SAE) {
+    console.log(num);
+    document.querySelector(".sae").innerHTML += "<div class='container-sae' id='" + num + "'><h2>" + num + "</h2><h3>Titre: " + SAE[num].titre + "</h3><a class='plus-btn' href='sae.html?sae=" + num + "'>En savoir plus</a></div>";
+}
