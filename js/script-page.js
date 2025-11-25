46027de8b181f438799897fe4c005116072c57eb
NobleExecutor <brakes_juniors.0t@icloud.com>
2025-11-25 14:24:47 +0100

    Changement complet du style, finit le script pour la page SAE

--- js/script-page.js
+++ js/script-page.js
@@ -1,12 +1,9 @@
-document.querySelector(".container").innerHTML = "<h1>" + SAE[id] + "</h1>"
+function loop(path) {
+    let result = "<br><h3>";
+    for (let ac in path) {
+        result += ac + ": " + path[ac] + "<br>";
+    }
+    return result += "</h3>";
+}
 
-
-/**
-document.querySelectorAll('.container-sae').forEach(div => {
-    console.log(div.id, SAE[div.id]);
-    div.innerHTML += "<h2>" + div.id + "</h2>";
-    div.innerHTML += "<h3>Titre: " + SAE[div.id].titre + "</h3>";
-    div.innerHTML += "<h3>Compétence(s): " + SAE[div.id].compétences + "</h3>";
-    div.innerHTML += "<a class='plus-btn' href='pages/" + div.id + ".html'>En savoir plus</a>";
-});
-**/
+document.querySelector("main").innerHTML = "<h1>" + sae + "</h1><h3>Semestre " + SAE[sae].semestre + "</h3><h2>Titre: " + SAE[sae].titre + "</h2><h2>Compétence(s): " + SAE[sae].compétences + "</h2><h2>Description: " + SAE[sae].description + "</h2><h2>Apprentissage critique: " + loop(SAE[sae].AC) + "</h2><h2>Ressources: " + loop(SAE[sae].ressources);
