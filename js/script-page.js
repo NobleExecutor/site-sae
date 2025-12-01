0542b99dd2289576ddf389bbab42d151a252a8b2
NobleExecutor <brakes_juniors.0t@icloud.com>
2025-12-01 10:38:35 +0100

    MàJ de script-page.js

--- js/script-page.js
+++ js/script-page.js
@@ -1,9 +1,9 @@
 function loop(path) {
-    let result = "<br><h3>";
+    let result = "<h3>";
     for (let ac in path) {
-        result += ac + ": " + path[ac] + "<br>";
+        result += "<a href='#' target='_blank'>" + ac + ": " + path[ac] + "</a><br>";
     }
     return result += "</h3>";
 }
 
-document.querySelector("main").innerHTML = "<h1>" + sae + "</h1><h3>Semestre " + SAE[sae].semestre + "</h3><h2>Titre: " + SAE[sae].titre + "</h2><h2>Compétence(s): " + SAE[sae].compétences + "</h2><h2>Description: " + SAE[sae].description + "</h2><h2>Apprentissage critique: " + loop(SAE[sae].AC) + "</h2><h2>Ressources: " + loop(SAE[sae].ressources);
+document.querySelector("main").innerHTML = "<div class='container'><h1 align='center'>" + sae + "</h1><h3 align='center'>Semestre " + SAE[sae].semestre + "</h3><h2>Titre: " + SAE[sae].titre + "</h2><h2>Compétence(s): " + SAE[sae].compétences + "</h2><h2>Description: " + SAE[sae].description + "</h2><h2>Apprentissage critique: " + loop(SAE[sae].AC) + "</h2><h2>Ressources: " + loop(SAE[sae].ressources) + "</div>";
