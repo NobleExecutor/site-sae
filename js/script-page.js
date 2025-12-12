13668025d0033c3ed2639ba6942ad28ac5042f5a
NobleExecutor <brakes_juniors.0t@icloud.com>
2025-12-12 18:59:23 +0100

    Synchro du style avec la branche main et suite du test du no uveau script

--- js/script-page.js
+++ js/script-page.js
@@ -1,11 +1,11 @@
 let v_sae = SAE[sae];
 let v_ac = SAE[sae].AC;
-let ac_contenu = "";
+let ac_contenu = "<br>";
 let v_res = SAE[sae].ressources;
-let res_contenu = "";
+let res_contenu = "<br>";
 
 for (let ac in v_ac) {
-    ac_contenu += ac + " - " + v_ac[ac] + "<br>";
+    ac_contenu += "<a class='ac-btn' href='#' target='_blank'>" + ac + " - " + v_ac[ac] + "</a><br>";
 }
 
 
@@ -16,11 +16,12 @@ for (let res in v_res) {
 
 let contenu = `<div class='container'>
                     <h1 align='center'>${sae} (Semestre ${v_sae.semestre})</h1>
-                    <h2 align='center'${v_sae.titre}</h2>
-                    <p>Description: ${v_sae.description}</p>
-                    <p>Compétence(s): ${v_sae.compétences}</p>
-                    <p>Apprentissages critiques couverts: ${ac_contenu}</p>
-                    <p>Liste des ressources mobilisées: ${res_contenu}</p>
+                    <h2 align='center'>${v_sae.titre}</h2>
+                    <p><span class='underline'>Description:</span> ${v_sae.description}</p>
+                    <p><span class='underline'>Compétence(s):</span> ${v_sae.compétences}</p>
+                    <p><span class='underline'>Apprentissages critiques couverts:</span> ${ac_contenu}</p>
+                    <p><span class='underline'>Liste des ressources mobilisées:</span> ${res_contenu}</p>
+                    <a class="return" href="index.html">Retourner vers la page principal</a>
                </div>`;
 
 document.querySelector("main").innerHTML = contenu;
