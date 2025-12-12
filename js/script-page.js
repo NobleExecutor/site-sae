55d87237f4e35e39e354fe490455fd18837208ce
NobleExecutor <brakes_juniors.0t@icloud.com>
2025-12-12 15:11:02 +0100

    Revert "Changements apportés au style et au script en conséquence"
    This reverts commit f42da0bb0075005b2b57383f93a439e9b7746819.

--- js/script-page.js
+++ js/script-page.js
@@ -1,9 +1,9 @@
 function loop(path) {
     let result = "<h3>";
     for (let ac in path) {
-        result += "<a class='ac-btn' href='#' target='_blank'>" + ac + " - " + path[ac] + "</a><br>";
+        result += "<a href='#' target='_blank'>" + ac + " - " + path[ac] + "</a><br>";
     }
     return result += "</h3>";
 }
 
-document.querySelector("main").innerHTML = "<div class='container'><h1 align='center'>" + sae + " (Semestre " + SAE[sae].semestre + ")</h1><h2 align='center'>" + SAE[sae].titre + "</h2><p><span class='underline'>Description:</span> " + SAE[sae].description + "</p><p><span class='underline'>Compétence(s):</span> " + SAE[sae].compétences + "</p><p><span class='underline'>Apprentissages critiques couverts:</span> " + loop(SAE[sae].AC) + "</p><p><span class='underline'>Liste des ressources mobilisées:</span> " + loop(SAE[sae].ressources) + "</p><a class='return' href='index.html'>Retourner vers la page principal</a></div>";
+document.querySelector("main").innerHTML = "<div class='container'><h1 align='center'>" + sae + " (Semestre " + SAE[sae].semestre + ")</h1><h2 align='center'>" + SAE[sae].titre + "</h2><p>Description: " + SAE[sae].description + "</p><p>Compétence(s): " + SAE[sae].compétences + "</p><p>Apprentissages critiques couverts: " + loop(SAE[sae].AC) + "</p><p>Liste des ressources mobilisées: " + loop(SAE[sae].ressources) + "</p></div>";
